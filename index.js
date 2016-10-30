ce377989a2df608353bfb3d82858fd5b95cfac69
杨益良 <yangyiliang@meituan.com>
2016-10-30 09:32:23 +0800

    去掉zIndex 和android下的漏缝问题

--- index.js
+++ index.js
@@ -7,57 +7,59 @@ import {
     View,
     TouchableOpacity,
     TouchableWithoutFeedback,
-    Text
+    Text,
+    Platform
 } from 'react-native';
 
 import Icon from 'react-native-vector-icons/FontAwesome';
-import {isFunction} from './utils';
+import {isFunction} from '../utils';
 
 class Select extends Component {
 
-  static propTypes = {
-      optionsData: React.PropTypes.array,
-      onSelect: React.PropTypes.func,
-      selectedOption: React.PropTypes.object,
-      topStyle: React.PropTypes.any,
-      placeholder: React.PropTypes.string
-  }
+    static propTypes = {
+        optionsData: React.PropTypes.array,
+        onSelect: React.PropTypes.func,
+        selectedOption: React.PropTypes.object,
+        topStyle: React.PropTypes.any,
+        placeholder: React.PropTypes.string
+    }
 
-  constructor(props) {
-      super(props);
-  
-      this.state = {
-        isOpen: false
-      };
-  }
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+            isOpen: false
+        };
+    }
 
-  close() {
-    this.setState({
-      isOpen: false
-    });
-  }
+    close() {
+        this.setState({
+            isOpen: false
+        });
+    }
 
-  _renderOptions() {
+    _renderOptions() {
         let {isOpen} = this.state;
         let {optionsData, selectedOption} = this.props;
         if(!isOpen) return null;
-
+        //解决android下option之间的间隙
+        let stylepatch = Platform.OS === 'ios' ? null : {marginTop: -1};
         let options =  optionsData.map((item, index)=>{
-          let check = null;
-          if(selectedOption.value === item.value) {
-            check = (
+            let check = null;
+            if(selectedOption.value === item.value) {
+                check = (
                     <View style={{position: 'absolute', right: 10,top: 14}}>
                         <Icon name='check' size={12} color="#000"/>
                     </View>
-            );
-          }
+                );
+            }
             return (
-                <TouchableWithoutFeedback key={index} onPress={this._itemPress.bind(this, item)}>
-                    <View style={styles.option}>
+                <TouchableOpacity key={index} onPress={this._itemPress.bind(this, item)}>
+                    <View style={[styles.option,stylepatch]}>
                         <Text style={styles.optionText}>{item.name}</Text>
                         {check}
                     </View>
-                </TouchableWithoutFeedback>
+                </TouchableOpacity>
             );
         })
         return (
@@ -76,7 +78,7 @@ class Select extends Component {
             selectedOption: item
         })
         if(isFunction(this.props.onSelect)) {
-          this.props.onSelect(item)
+            this.props.onSelect(item)
         }
     }
 
@@ -110,7 +112,6 @@ const styles = StyleSheet.create({
     select: {
         flex: 1,
         height: 40,
-        zIndex: 1000
     },
 
     option: {
@@ -121,14 +122,14 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderTopWidth: 0,
         borderColor: '#ececec'
+
     },
     optionText: {
         flex: 1,
         textAlign: 'center'
-       
     },
     optionBtn: {
-      borderWidth: 1
+        borderWidth: 1
     }
 });
 
